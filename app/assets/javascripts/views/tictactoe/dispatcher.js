668f82db45aa31c407a442dbc6bf708a1ad45da8
Greg Pallotta <gregpallotta@gmail.com>
2013-07-09 21:16:38 -0400

    add chat

--- app/assets/javascripts/views/tictactoe/dispatcher.js
+++ app/assets/javascripts/views/tictactoe/dispatcher.js
@@ -24,6 +24,7 @@ Games.Views.TictactoeDispatcher = Backbone.View.extend({
   humanGame: function() {
     var view = new Games.Views.TictactoeHuman();
     $('#main-container').html(view.render().el);
+    this.setUpMessages();
   },
 
   gregbotGame: function() {
@@ -35,6 +36,24 @@ Games.Views.TictactoeDispatcher = Backbone.View.extend({
   hideButtons: function() {
     $('#ttt-human').hide();
     $('#ttt-gregbot').hide();
+  },
+
+  setUpMessages: function() {
+    var that = this;
+    this.messagesRef = new Firebase('https://games-by-greg.firebaseIO.com/messages');
+    this.messagesRef.limit(20).on('child_added', function(snapshot) {
+      var message = snapshot.val();
+      var html = '<div><strong>' + message.name + '</strong>: ' + message.message + '</div>';
+      $('#chatbox').prepend(html);
+    });
+    $('#messageInput').keyup(function (e) {
+      if (e.keyCode == 13) {
+        var name = $('#nameInput').val();
+        var message = $('#messageInput').val();
+        that.messagesRef.push({name:name, message:message});
+        $('#messageInput').val('');
+      }
+    });
   }
 
 });
